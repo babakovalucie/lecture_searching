dbcfb24082cd3301f2c983892fe81c3931117000
babakovalucie <lucie7705@gmail.com>
2026-04-15 09:15:43 +0200

    jedna

--- searching.py
+++ searching.py
@@ -21,11 +21,16 @@ def read_data(file_name, field):
     cwd_path = Path.cwd()
     
     file_path = cwd_path / file_name
-
+    with open(file_path, mode="r") as file:
+        data = json.load(file)
+    if field in data.keys():
+        return data[field]
+    else:
+        return None
 
 def main():
-    pass
-
+    my_data = read_data("sequential.json","unordered_numbers")
+    print(my_data)
 
 if __name__ == "__main__":
     main()
