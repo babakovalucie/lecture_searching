e4e40344ed2af43104893e046763ab29d20b2785
babakovalucie <lucie7705@gmail.com>
2026-04-15 09:33:55 +0200

    treba druhy commit

--- searching.py
+++ searching.py
@@ -28,9 +28,25 @@ def read_data(file_name, field):
     else:
         return None
 
+def linear_search(searched_data, searched_number):
+    positions = []
+    count = 0
+    idx = 0
+    searched_dictionary = {}
+    while idx < len(searched_data):
+        if searched_data[idx] == searched_number:
+            count += 1
+            positions.append(idx)
+        idx += 1
+
+    searched_dictionary["positions"] = positions
+    searched_dictionary["count"] = count
+    return searched_dictionary
+
 def main():
     my_data = read_data("sequential.json","unordered_numbers")
     print(my_data)
-
+    found_numbers = linear_search(my_data,5)
+    print(found_numbers)
 if __name__ == "__main__":
     main()
